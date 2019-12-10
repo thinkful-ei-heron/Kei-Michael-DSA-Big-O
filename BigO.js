a07d40e2a4edcfac62e97c38fe41b4f6a7e7be06
Keisuke Isobe <keisuke.isobe.2011@gmail.com>
2019-12-10 16:52:55 +0000

    up through hanoi algorithm

---
+++ BigO.js
@@ -0,0 +1,42 @@
+// 1-1. Because you are only yelling out once, and the call will work no matter how many people there
+// are, it is constant time. O(1)
+// 1-2. This is linear, because you are doing one operation for each person, which means it grows in 
+// linear proportion to the number of people. O(n)
+// 2. This is constant because there is only a constant number of operations happening (modullo, this is 
+// assuming that modullo is a constant time operation)
+// 3. This is O(n*m) because there are two arrays of different lengths (n and m), and we are iterating 
+// through one array and at every index of the outer array we are iterating through the entire inner array. 
+// 4. This is O(n) because we are iterating once through the given array (length n). 
+// 5. This is O(n) because we are iterating once through the given array (best case O(1), worst case O(n), 
+// average case O(n))
+// 6. This is O(n^2) because there is a pair of nested for loops, although the inner loop will run a smaller
+// and smaller number of times on each iteration of the outer loop it still does not decrease the running time
+// to hit anything lower than O(n^2).
+// 7. This is O(n) because the for loop is simply iterating once (the value of n times), all operations inside
+// the for loop are constant time (check value, subtract, or close enough probably (push)).
+// 8. Everything inside the while loop runs in constant time. At worst the array will be halved log(n) times,
+// so the running time is O(logn).
+// 9. Assuming that Math.random is a constant time operation, this is O(1).
+// 10. This is O(n) because it is checking every incremental whole number up to n starting at 2 and then doing
+// some constant time operation (modullo) within the for loop (finding prime numbers).
+
+//11. 
+
+
+function hanoi(disks, origin, destination, extra){
+  if (disks === 1) {
+    console.log(`Disc ${disks} moved from ${origin} to ${destination}`);
+
+  } else {
+    hanoi(disks-1, origin, extra, destination);
+    console.log(`Disc ${disks} moved from ${origin} to ${destination}`);
+    hanoi(disks-1, extra, destination, origin);
+  }
+}
+hanoi(2, 'a', 'b', 'c'); //3
+console.log('\b');
+hanoi(3, 'a', 'b', 'c'); //7
+console.log('\b');
+hanoi(4, 'a', 'b', 'c'); //15
+console.log('\b');
+hanoi(5, 'a', 'b', 'c'); //
